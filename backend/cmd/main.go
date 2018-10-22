91b6b32491736f312acedc324828e336609e14f4
Willi Schinmeyer <willi.schinmeyer@freiheit.com>
2018-10-22 19:07:40 +0200

    add sign in button (does nothing)

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
+	"time"
 )
 
 const mockJSON = `{
@@ -18,8 +20,22 @@ const mockJSON = `{
 	]
 }`
 
+const index = `<html>
+	<head>
+		<title>Directory - Sign In</title>
+	</head>
+	<body>
+		<a href="https://slack.com/oauth/authorize?scope=identity.basic&client_id=3419777384.461042975377"><img alt="Sign in with Slack" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>
+	</body>
+</html>
+`
+
 func main() {
+	start := time.Now()
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
+		http.ServeContent(rw, req, "/index.html", start, strings.NewReader(index))
+	})
 	mux.HandleFunc("/users.json", func(rw http.ResponseWriter, req *http.Request) {
 		if req.Method != http.MethodGet {
 			http.Error(rw, "use GET", http.StatusMethodNotAllowed)
